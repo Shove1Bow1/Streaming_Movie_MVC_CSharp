cd4b9c01bf8dab53a6d1eb13269b8f82c4e54059
Shov1bow1 <fireblaster2001@gmail.com>
2022-04-09 08:41:13 +0700

    Log in for admin

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Streaming_Video_MVC.Models;
+using System.Web;
+using System.Linq;
 namespace Streaming_Video_MVC.Controllers
 {
     public class AdminController : Controller
@@ -11,13 +13,20 @@ namespace Streaming_Video_MVC.Controllers
         }
     
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Index(string Name,string Password)
         {
+            if (HttpContext.Session.GetString("AdminId") != null)
+            {
+                return View("Hello");
+            }
             if (Name != null && Password != null)
             {
                var data=webnangcaoContext.Admins.Where(s => s.Name.Equals(Name)&& s.Password.Equals(Password)).ToList();
                if (data.Count > 0)
                {
+                   HttpContext.Session.SetString("AdminId",data[0].IdAdmin);
+                   HttpContext.Session.SetString("AdminName", data[0].Name);
                    return View("Hello");
                }
             }
@@ -25,11 +34,22 @@ namespace Streaming_Video_MVC.Controllers
         }
         public IActionResult Hello()
         {
+            if (HttpContext.Session.GetString("AdminId") == null && HttpContext.Session.GetString("AdminName")==null)
+            {
+                return View("Index");
+            }
             return View();
         }
         public IActionResult login()
         {
             return View();
         }
+        public IActionResult DeleteSession()
+        {
+            HttpContext.Session.Remove("AdminId");
+            HttpContext.Session.Remove("AdminName");
+            return RedirectToAction("Index");
+        }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -2,8 +2,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDistributedMemoryCache();
 
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromDays(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -17,11 +25,12 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseSession();
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Admin}/{action=Index}/{id?}");
 
 app.Run();
+
